043e573614805d708ce15c201cce7ca06ccb9da6
bandanica <65416715+bandanica@users.noreply.github.com>
2022-02-02 18:03:24 +0100

    added doctrine and codeigniter4

---
+++ project-root/app/Config/Doctrine.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Config;
+
+use CodeIgniter\Config\BaseConfig;
+
+class Doctrine extends \Daycry\Doctrine\Config\Doctrine
+{
+    public $debug = false;
+
+    // see doc https://www.doctrine-project.org/projects/doctrine-orm/en/2.8/reference/advanced-configuration.html#auto-generating-proxy-classes-optional
+    public $setAutoGenerateProxyClasses = ENVIRONMENT === 'development' ? true : false;
+
+    /*
+     * Methods "Redis, Memcached" or "null"
+     */
+    public $cache = null;
+
+    /*
+     * Ports
+     * for Redis : 6379
+     * for Memcached : 11211
+     */
+    public $portCache = null;
+
+    public $hostCache = null;
+
+    /*
+     * Index of redis database
+     */
+    public $databaseRedis = 0;
+
+    /*
+     * Namespace of Redis database or name of Memcached server
+     */
+    public $namespaceCache = "name";
+    
+    /*
+     * Namespace and folder of models
+     */
+    public $namespaceModel = 'App/Models';
+    public $folderModel = APPPATH . 'Models';
+
+    /*
+     * Namespace and folder of proxies
+     */
+    public $namespaceProxy = 'App/Models/Proxies';
+    public $folderProxy = APPPATH . 'Models/Proxies';
+
+        /*
+     * Folder for entities
+     */
+    public $folderEntity = APPPATH . 'Models/Entity';
+}
